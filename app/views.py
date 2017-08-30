4d6567ac47e57d477f996e44b5b96708f97098f2
webrobbie <misterdi@protonmail.com>
2017-08-30 19:39:14 +0800

    NA

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,flash,request
 from flask_login import login_user,logout_user,login_required,current_user
-from flask_admin.contrib.sqla import ModelView
+# from flask_admin.contrib.sqla import ModelView
 from datetime import datetime
 import re,operator
 from . import app,db,admin
